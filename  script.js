9fa61fcbc4736e75ddf1f2e912190feefd8639ab
STGMM <santiago.marquez@escola.pr.gov.br>
2025-10-29 12:09:32 -0300

    Add files via upload

---
+++  script.js
@@ -0,0 +1,55 @@
+import{PALAVRAS_RUINS} from "./palavrasRuins"
+
+const botaoMostraPalavra=document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavra.addEventListener('click', mostraPalavraschave);
+
+function mostraPalavraschave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado= document.querySelector('#resultado-palavrachave');
+    const palavras=processaTexto(texto);
+
+    campoResultado.textcontent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+    let Palavras=texto.split(/\P(L)=/u);
+    for(let i in palavras){
+    palavras[i] = palavras [i].tolowerCase();
+}
+
+palavras=tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort();
+function ordenaPalavra(p1,p2){
+    return frequencias[p2]-frequencias[p1];
+}
+
+    return palavras.slice(0,10);
+}
+
+function contraFrequencias(palavras){
+    let frequencias={};
+    for (let i of palavras){
+        frequencia [i]=0;
+        for(let j of palavras){
+            if(i==j){
+                frequencias[i]++;
+            }
+        }
+    }
+    
+return frequencias;
+    
+}
+
+function tiraPalavrasRuins(palavras){
+
+              for(let palavra of palavras){
+        if(PALAVRAS_RUINS.has(palavra) && palavra.length>2){
+           palavrasBoas.push(palavra);
+        }
+     }
+     return palavrasBoas
+}
